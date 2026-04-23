388946d430b5052d8ea96e32151dbcc65a17c747
hojin <hjin9445@gmail.com>
2026-04-23 16:46:52 +0900

    fix: variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,9 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.1-rc3" # 최신 안정 버전 권장
-    }
-  }
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
 }
 
-provider "proxmox" {
-  pm_api_url          = "https://192.168.x.x:8006/api2/json" # Proxmox 서버 IP로 수정
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true # 홈랩이므로 Self-signed 인증서 허용
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
 }
