e4a7ef343e5d9bc3c71e2e1913c2ffc381971ab4
hojin <hjin9445@gmail.com>
2026-04-27 13:12:02 +0900

    fix: providers 3.x -> 2.9.14 and terraform.yml init -upgrade option

--- providers.tf
+++ providers.tf
@@ -2,16 +2,14 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "3.0.1-rc1" 
+      version = "2.9.14"
     }
   }
 }
 
 provider "proxmox" {
-  # 💡 변수를 통하지 않고 직접 값을 박아 넣습니다.
-  pm_api_url          = "https://100.78.176.123:8006/api2/json"
-  pm_api_token_id     = "terraform-prov@pve!Terraform" 
-  
-  pm_api_token_secret = var.proxmox_api_token_secret 
-  pm_tls_insecure     = true 
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = true # 개발 환경용
 }
