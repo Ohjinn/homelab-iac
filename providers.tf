d15e59111b65b5d772d0ad9b6396405aad17dd7e
hojin <hjin9445@gmail.com>
2026-04-27 13:05:45 +0900

    fix: token_id hard coding

--- providers.tf
+++ providers.tf
@@ -2,14 +2,16 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "3.0.1-rc1" # 사용 중인 버전 확인
+      version = "3.0.1-rc1" 
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true # 개발 환경용
+  # 💡 변수를 통하지 않고 직접 값을 박아 넣습니다.
+  pm_api_url          = "https://100.78.176.123:8006/api2/json"
+  pm_api_token_id     = "terraform-prov@pve!Terraform" 
+  
+  pm_api_token_secret = var.proxmox_api_token_secret 
+  pm_tls_insecure     = true 
 }
